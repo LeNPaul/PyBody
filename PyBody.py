47f7e9ec4fc4e8ea999c9a1d9cff31e8c59aa6ea
LeNPaul <l.nguyen.paul@gmail.com>
2016-05-11 01:44:28 -0400

    Add code from previous project as start point

--- PyBody.py
+++ PyBody.py
@@ -1,33 +1,35 @@
+#set start screen
+#lump everything under removeParticle function (escape velocity, distances limit)
+#Integrator
 import math
 import random
 import timeit
 import pygame
+import Universe
 
-#set start screen
-#lump everything under removeParticle function (escape velocity, distances limit)
-#Integrator
-
-black = (0,0,0)
-white = (255,255,255)
-
-#Setting up PyGame
+#Setting up pygame
 
 start = timeit.default_timer()
 
 totaltime = 0
 
-displayWidth = 1200
-displayHeight = 700
+displayWidth = 700
+displayHeight = 400
+
+particlenumber = 100
+centralmass = Universe.sun
 
 clock = pygame.time.Clock()
 
 simulationDisplay = pygame.display.set_mode((displayWidth, displayHeight))
-pygame.display.set_caption("PyBody Simulation")
+pygame.display.set_caption("Galaxy Simulation")
 pygame.display.update()
 
 simulationExit = False
 
 #Simulation loop
+
+Universe.generateparticles(particlenumber)
 
 while not simulationExit:
     for event in pygame.event.get():
@@ -38,9 +40,44 @@ while not simulationExit:
 
             simulationExit = True
 
-    simulationDisplay.fill(white)
+            #Output final simulation data into a text file
 
+            fw = open("SimulationData.txt", "w")
+            fw.write("Day Number: " + str(totaltime) + "\n")
+            
+            for i in Universe.particlelist:
+                x = str(i.px)
+                y = str(i.py)
+                fw.write(x + "," + y + "\n") 
+            fw.close()
+
+    simulationDisplay.fill(Universe.black)
+
+    #Universe.particlelist.append(Universe.sun)
+
+    for particle in Universe.particlelist:
+        for other in filter(lambda p: p != particle, Universe.particlelist):
+            Universe.integrator(particle,other)
+            Universe.drag(particle)
+
+        #x = int(displayWidth/2 + particle.px/Universe.AU*500)
+        #y = int(displayHeight/2 + particle.py/Universe.AU*500)
+        x = int(particle.px)
+        y = int(particle.py)
+        
+        pygame.draw.circle(simulationDisplay,Universe.white,(x,y),1,1)
+
+    print len(Universe.particlelist)
     pygame.display.update()
+
+    totaltime += 1
+
+    #Print the number of days passed in simulation
+    print "Day: %d" %totaltime
+
+    clock.tick(30)
+
+print timeit.default_timer() - start, "seconds"
 
 pygame.quit()
 quit()
--- Universe.py
+++ Universe.py
@@ -4,6 +4,104 @@ import random
 #Colours
 
 black = (0,0,0)
-white(255,255,255)
+white = (255,255,255)
 green = (0,255,0)
 yellow = (255,255,0)
+
+#Universal constants
+
+particlelist = []
+
+#Constants
+
+G = 6.67428e-11
+AU = (149.6e6 * 1000)
+dt = 24*3600
+
+msun = 1.98892 * 10**30
+mearth = 5.9742 * 10**24
+dearth = -1*AU
+
+#Parameters
+
+class Particle(object):
+    def __init__(self, mass, px, py, vx, vy):
+
+        self.mass = mass
+
+        self.px = px
+        self.py = py
+        self.vx = vx
+        self.vy = vy
+
+def integrator(self,other):
+
+    #Returns the position and velocity of self object
+    #Uses Euler numerical integration
+
+    sx = self.px
+    sy = self.py
+    ox = other.px
+    oy = other.py
+
+    dx = (ox - sx)
+    dy = (oy - sy)
+    d = math.sqrt(dx**2 + dy**2)
+
+    f = G*self.mass*other.mass / (d**2)
+
+    theta = math.atan2(dy,dx)
+    fx = math.cos(theta) * f
+    fy = math.sin(theta) * f
+
+    #Output
+    self.vx += fx/self.mass * dt
+    self.vy += fy/self.mass * dt
+
+    self.px += self.vx * dt
+    self.py += self.vy * dt
+
+def drag(self):
+
+        self.vx = self.vx*0.9
+        self.vy = self.vy*0.9
+
+#Initial massive objects
+sun = Particle(msun,0,0,0,0)
+earth = Particle(mearth,-dearth,0,0,29.783 * 1000)
+
+#Generating particles
+
+def generateparticles(particlenumber):
+
+    for n in range(particlenumber):
+        #r = random.randint(0.2*AU,0.25*AU)
+        #theta = random.uniform(0,6.3)
+        #px = int(r*math.sin(theta))
+        #py = int(r*math.cos(theta))
+        px = random.randint(0,700)
+        py = random.randint(0,400)
+        vx = 0#random.randint(0,100)*random.randint(-1,1)
+        vy = 0#random.randint(0,100)*random.randint(-1,1)
+        mass = random.randint(100,1000)
+
+        particle = Particle(mass, px, py, vx, vy)
+
+        particlelist.append(particle)
+        
+#Merge function
+
+def merge(self,other, particlelist,i,p):
+
+    if self.px == other.px and self.py == other.py:
+        px = self.px
+        py = self.py
+        vx = self.vx + other.vx
+        vy = self.vy + other.vy
+        mass = self.mass + other.mass
+        particlelist.pop(i)
+        particlelist.pop(p)
+
+        particle = Particle(mass, px, py, vx, vy)
+        particlelist.append(particle)
+        print "THIS SHIT WORKS"
