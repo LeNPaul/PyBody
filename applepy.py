b1d53ea44888684a05e94bf3ae345e6ab6d95ae1
LeNPaul <l.nguyen.paul@gmail.com>
2016-05-24 14:45:53 -0400

    Add button function

--- applepy.py
+++ applepy.py
@@ -52,6 +52,21 @@ functionKeys = {
     pygame.K_MINUS:  (lambda x: x.zoom(0.5)),
     pygame.K_z:      (lambda x: x.reset())}
 
+#Button function
+
+def button(msg,x,y,w,h,ic,ac):
+    mouse = pygame.mouse.get_pos()
+
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    smallText = pygame.font.Font("freesansbold.ttf",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
+
 #Diplay number of days passed
 
 def daysPassed(count):
