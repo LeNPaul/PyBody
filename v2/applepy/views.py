feba6a358a7cacebca52f2fc04bec3d449b86cde
Paul Le <l.nguyen.paul@gmail.com>
2018-02-21 12:32:47 -0500

    Add starter web pages

--- v2/applepy/views.py
+++ v2/applepy/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# Create your views here.
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
